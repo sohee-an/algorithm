5c9183396c8f8494ee7eba89e1999b1ce32a09d0
sohee-an <soan32@hanmail.net>
2024-06-13 17:08:27 +0900

    완전탐색: 2468

--- 그래프/완전탐색/2468.py
+++ 그래프/완전탐색/2468.py
@@ -1,29 +1,55 @@
 # 2-c 안전영역
+from collections import deque
 n = int(input())
 graph = []
 tree=[]
 maxNum = 0
 
-dx = [0 ,0, 1, -1]
-dy = [1, -1, 0 ,0]
+dy=[-1,0,1,0]
+dx=[0,1,0,-1]
 
 for i in range(n):
     graph.append(list(map(int, input().split())))
     for j in range(n):
         if graph[i][j] > maxNum:
             maxNum = graph[i][j] 
+            
+def dfs(num,x,y,visited):
+    
+    q=deque()
+    q.append((x,y))
+    visited[x][y]=1
+  
+    
+    while q :
+        x,y=q.pop()
+        
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            
+            if 0<=nx<n and 0<=ny<n and visited[nx][ny]==0 and graph[nx][ny] >num:
+                visited[nx][ny]=1
+                q.append((nx,ny))
+                
+    
 
 
 
 
-result =0;
+result =[]
 
 
 for i in range(maxNum):
-    visited = [[0] * n for _ in range(n)]
     cnt=0
-    for i in range(n):
-        for j in range(n):
-            if graph[j][k]>i and visited[j][k]==0
-            bfs(j,k,i,visited)
+    visited = [[0] * n for _ in range(n)]
+    
+    for j in range(n):
+        for k in range(n):
+            if  graph[j][k] > i and visited[j][k]==0:
+                cnt+=1
+                dfs(i,j,k,visited)
+    result.append(cnt)
+                
+print(max(result))
                 
