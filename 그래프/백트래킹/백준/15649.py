fc4f360f13ed7e20482d55bf0363d0c15b4f556f
sohee-an <soan32@hanmail.net>
2025-09-27 16:38:52 +0900

    백준 :15649

---
+++ 그래프/백트래킹/백준/15649.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+visited = [False] * (n+1)
+path = []
+
+def dfs(depth):
+    if depth == m:
+        print(*path)
+        return
+    for i in range(1, n+1):
+        if not visited[i]:
+            visited[i] = True
+            path.append(i)
+
+            dfs(depth+1)
+
+            path.pop()          # 상태 복구
+            visited[i] = False  # 상태 복구
+
+dfs(0)
