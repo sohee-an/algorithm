8d4b2771025a61f52ff139b066946e3f1f810f5c
sohee-an <soan32@hanmail.net>
2025-10-17 15:19:58 +0900

    백준: 2579 다시풀기

--- dp/2579.py
+++ dp/2579.py
@@ -14,9 +14,11 @@ if n == 1:
 
 dp = [0] * (n + 1)
 dp[1] = stair[1]
-dp[2] = stair[1] + stair[2]
+# dp[2] = stair[1] + stair[2]
+dp[2] = max(stair[1] + stair[2], stair[2])
 
 for i in range(3, n + 1):
-    dp[i] = max(dp[i-2] + stair[i], dp[i-3] + stair[i-1] + stair[i])
+    dp[i] = max(dp[i-2] + stair[i], 
+                dp[i-3] + stair[i-1] + stair[i])
 
 print(dp[n])
