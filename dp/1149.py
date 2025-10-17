dfdde1975a434edd3b71bb3c1965c4f49a405ef9
sohee-an <soan32@hanmail.net>
2025-10-17 15:58:48 +0900

    백준: 1149

--- dp/1149.py
+++ dp/1149.py
@@ -6,9 +6,21 @@
 # N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
 # i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
 # 겹치지않게 최소값
-n= int(input())
-arr=[]
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-for i in range(n):
-    arr.append(list(map(int,input().split())))
+dp = [[0] * 3 for _ in range(n)]
+
+# 초기값 설정 (첫 번째 집)
+dp[0][0] = arr[0][0]
+dp[0][1] = arr[0][1]
+dp[0][2] = arr[0][2]
+
+# 두 번째 집부터 (i = 1)
+for i in range(1, n):
+    dp[i][0] = arr[i][0] + min(dp[i-1][1], dp[i-1][2])
+    dp[i][1] = arr[i][1] + min(dp[i-1][0], dp[i-1][2])
+    dp[i][2] = arr[i][2] + min(dp[i-1][0], dp[i-1][1])
+
+print(min(dp[n-1][0], dp[n-1][1], dp[n-1][2]))
 
